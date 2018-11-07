5ec83fa2961f2e4ff6eb0b485faf164faeca530a
Edgar Burelo <edgar.burelo@gmail.com>
2018-11-07 14:58:15 -0600

    adding

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,116 @@
 let friendArr = [
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
-        ]
-    }
+        {
+                "name": "Ahmed",
+                "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+                "scores": [
+                        5,
+                        1,
+                        4,
+                        4,
+                        5,
+                        1,
+                        2,
+                        5,
+                        4,
+                        1
+                ]
+        },
+        {
+                "name": "cesar",
+                "photo": "https://pixel.nymag.com/imgs/daily/vulture/2018/06/15/15-mark-zuckerberg.w700.h700.jpg",
+                "scores": [
+                        "3",
+                        "4",
+                        "2",
+                        "2",
+                        "3",
+                        "4",
+                        "3",
+                        "2",
+                        "3",
+                        "3"
+                ]
+        },
+        {
+                "name": "hide the pain harold",
+                "photo": "https://i.chzbgr.com/full/8334924288/hCFACD046/",
+                "scores": [
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1",
+                        "1"
+                ]
+        },
+        {
+                "name": "John",
+                "photo": "https://i.amz.mshcdn.com/niKEQtUKLRjVeprnNwCfQGnBc78=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F511986%2F56fe7642-b053-4667-89dc-e2d3abea5333.jpg",
+                "scores": [
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "4",
+                        "3",
+                        "2",
+                        "1",
+                        "1"
+                ]
+        },
+        {
+                "name": "Angry Business man",
+                "photo": "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4649705.jpg",
+                "scores": [
+                        "1",
+                        "4",
+                        "3",
+                        "2",
+                        "4",
+                        "2",
+                        "2",
+                        "3",
+                        "1",
+                        "2"
+                ]
+        },
+        {
+                "name": "Crazy Grandma",
+                "photo": "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg",
+                "scores": [
+                        "4",
+                        "3",
+                        "4",
+                        "4",
+                        "2",
+                        "3",
+                        "2",
+                        "5",
+                        "4",
+                        "4"
+                ]
+        },
+        {
+                "name": "Heroin Enthusiast",
+                "photo": "https://vignette.wikia.nocookie.net/steven-universe/images/1/17/Weird_stock_photo.jpeg/revision/latest?cb=20170412213628",
+                "scores": [
+                        "4",
+                        "4",
+                        "4",
+                        "2",
+                        "3",
+                        "3",
+                        "2",
+                        "3",
+                        "2",
+                        "1"
+                ]
+        }
 ];
 
 module.exports = friendArr;
